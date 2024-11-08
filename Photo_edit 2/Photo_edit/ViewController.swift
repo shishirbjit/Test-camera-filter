7d7766f135b0adcdf9c3c45727a2a4748fe28c13
zulfiker shishir <ali.zulfiker@bjitgroup.com>
2024-11-08 17:16:43 +0600

    first commit with initial code

---
+++ Photo_edit 2/Photo_edit/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Photo_edit
+//
+//  Created by Ekramul Hoque on 8/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func picImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: PhotoEditorDelegate {
+    
+    func doneEditing(image: UIImage) {
+        imageView.image = image
+    }
+    
+    func canceledEditing() {
+        print("Canceled")
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Local variable inserted by Swift 4.2 migrator.
+        
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+        
+        
+        guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage else {
+            picker.dismiss(animated: true, completion: nil)
+            return
+        }
+        picker.dismiss(animated: true, completion: nil)
+        
+        
+        let photoEditor = PhotoEditorViewController(nibName:"PhotoEditorViewController",bundle: Bundle(for: PhotoEditorViewController.self))
+        photoEditor.photoEditorDelegate = self
+        photoEditor.image = image
+        //Colors for drawing and Text, If not set default values will be used
+        //photoEditor.colors = [.red, .blue, .green]
+        
+       
+            
+        for i in 1...10 {
+            photoEditor.stickers.append(UIImage(named: i.description )!)
+        }
+
+        
+        //To hide controls - array of enum control
+        //photoEditor.hiddenControls = [.crop, .draw, .share]
+        photoEditor.modalPresentationStyle = UIModalPresentationStyle.currentContext //or .overFullScreen for transparency
+        present(photoEditor, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+    return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
+
+
+
+
